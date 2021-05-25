e4c2c90c2b60cae95de01552477a04cc0b85e004
DeLaCruz26 <dametrik.fick@gmail.com>
2021-05-24 21:26:02 -0400

    day 1 of 100daysofswift

---
+++ Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+//Variables
+var greeting = "Hello, playground"
+greeting = "Goodbye"
+//Strings and integers
+var age = 26
+var population = 8_000_000
+//Multi-line strings
+var str = """
+    This goes
+    over multiple
+    lines
+    """
+var str2 = """
+    This goes \
+    over multiple \
+    lines
+    """
+//Doubles and Booleans
+var pi = 3.141
+var awesome = true
+//String interpolation
+var score = 85
+var string = "Your score was \(score)"
+var results = "The test results are in: \(string)"
+//Constants
+let taylor = "swift"
+//Type annotations
+let string1 = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
